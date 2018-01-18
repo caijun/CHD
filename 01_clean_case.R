a3b7507a4a78d8454025c4f3f37e9256a9eb541d
caijun <tony.tsai.2046@gmail.com>
2018-01-18 17:56:35 +0800

    read data directly from xlsx

--- 01_clean_case.R
+++ 01_clean_case.R
@@ -1,24 +1,37 @@
 rm(list = ls())
-setwd("~/Downloads/FengYu/")
 
 source("src/helper.R")
 
-library(plyr)
 library(tidyverse)
-
-dat.nanjing <- read.csv("dat/case.csv", fileEncoding = "utf8")
-# 编号2133不存在
-dat.nanjing$编号 <- paste0("nj", dat.nanjing$编号)
-dat.nanjing$血型 <- NULL
-dat.suzhou <- read.csv("dat/case_suzhou.csv", fileEncoding = "utf8")
-dat.suzhou$编号 <- paste0("sz", dat.suzhou$编号)
-dat.suzhou$血型 <- NULL
-dat.nanjing1 <- read.csv("dat/case_new.csv", fileEncoding = "utf8")
-dat.nanjing1$编号 <- paste0("nj_new", dat.nanjing1$编号)
-dat <- rbind(dat.nanjing, dat.suzhou, dat.nanjing1)
-write.csv(dat, file = "dat/case_combine.csv", fileEncoding = "utf8", row.names = F)
 library(xlsx)
-write.xlsx(dat, file = "dat/case_combine.xlsx")
+
+# combine multiple data files into one -----------------------------------------
+dat.nj <- read.xlsx("~/Downloads/FengYu/raw/网上手术登记11-1(diagnosis).xls", 
+                    sheetName = "case")
+# 编号2133不存在
+dat.nj$编号 <- paste0("nj", dat.nj$编号)
+dat.nj$血型 <- NULL
+
+dat.sz <- read.xlsx("~/Downloads/FengYu/raw/苏州儿童医院数据.xls", 
+                    sheetName = "case")
+dat.sz$编号 <- paste0("sz", dat.sz$编号)
+dat.sz$血型 <- NULL
+
+dat.nj1 <- read.xlsx("~/Downloads/FengYu/raw/新增(diagnosis).xls", 
+                     sheetName = "case")
+dat.nj1$编号 <- paste0("nj_new", dat.nj1$编号)
+
+dat.nj2 <- read.xlsx("~/Downloads/FengYu/raw/2017 update.xlsx", 
+                     sheetName = "case")
+dat.nj2$编号 <- paste0("nj_2017new", dat.nj2$编号)
+
+setdiff(names(dat.nj), names(dat.nj2))
+setdiff(names(dat.nj2), names(dat.nj))
+
+dat <- rbind(dat.nj, dat.sz, dat.nj1, dat.nj2)
+write.xlsx(dat, file = "output/case_combine.xlsx")
+
+# check each variable ----------------------------------------------------------
 names(dat)
 names(dat) <- c("id" # 编号
                 # , "blood.type" # 血型
